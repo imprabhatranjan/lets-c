2113c232858a87646bfdb977643e86bd45199a32
Prahat Ranjan Choudhary <imprabhatranjan@gmail.com>
2024-08-22 13:18:53 +0530

    updated code

--- hello.c
+++ hello.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+//its my first code block,while learning c programming
+
 int main() {
 
-printf("Hello World!");
+printf("Hello World!\n\n");
+
+printf("Have a good day, Prabhat!");
 
 return 0;
 }
---
+++ integer.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int prabhat = 50;
+{
+
+printf("}%d \n, prabhat);
+}
